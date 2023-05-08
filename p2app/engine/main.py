8bf5a19227d3e05c2b67216661b1e6fcf04ee8bf
Mason Wong <masonjw1@uci.edu>
2023-05-07 18:11:56 -0700

    Fixing Event Sending from Engine
    Deleted the applicationlevelevents.py module. Also added () to the end of every event that was sent to the engine, thats why nothing was working in the user interface. Also made three functions, is_sqlite_database, _quietly_execute_statement, and _display_error to make sure that the file is a database and that it is properly being connected to.

--- p2app/engine/main.py
+++ p2app/engine/main.py
@@ -11,8 +11,8 @@
 
 import sqlite3
 from p2app.events.app import QuitInitiatedEvent, EndApplicationEvent
-from p2app.events.database import OpenDatabaseEvent, CloseDatabaseEvent
-from applicationlevelevents import opendatabaseevent, _disconnect
+from p2app.events.database import OpenDatabaseEvent, CloseDatabaseEvent,\
+    DatabaseOpenedEvent, DatabaseOpenFailedEvent, DatabaseClosedEvent
 
 
 class Engine:
@@ -34,13 +34,46 @@ class Engine:
         # This is a way to write a generator function that always yields zero values.
         # You'll want to remove this and replace it with your own code, once you start
         # writing your engine, but this at least allows the program to run.
-        print(event, type(event))
         if type(event) == QuitInitiatedEvent:
-            yield EndApplicationEvent
-            exit()
+            yield EndApplicationEvent()
 
         elif type(event) == OpenDatabaseEvent:
-            opendatabaseevent(event)
+            database_path = event.path()
+            db_checker = is_sqlite_database(database_path)
+            try:
+                if db_checker:
+                    connection = sqlite3.connect(database_path, isolation_level = None)
+                    _quietly_execute_statement(connection, 'PRAGMA foreign_keys = ON;')
+                    yield DatabaseOpenedEvent(database_path)
+                else:
+                    yield DatabaseOpenFailedEvent('not a database')
+            except sqlite3.Error as e:
+                _display_error(e)
 
-        # elif type(event) == CloseDatabaseEvent:
-        #     _disconnect(connection)
+        elif type(event) == CloseDatabaseEvent:
+            yield DatabaseClosedEvent()
+
+def is_sqlite_database(database_path):
+    """Checks if file is a database"""
+    with open(database_path, "rb") as file:
+        header = file.read(16)
+        return header.startswith(b"SQLite format 3")
+
+
+def _quietly_execute_statement(connection, statement):
+    """Executes a statement quietly"""
+    cursor = None
+    try:
+        # Execute a statement that we expect not to return any data.
+        cursor = connection.execute(statement)
+    finally:
+        # When we get here, cursor might be None (if executing the statement
+        # failed), so we need to be careful to close the cursor only if it
+        # was opened.
+        if cursor is not None:
+            cursor.close()
+
+
+def _display_error(e):
+    error_message = ' '.join(e.args)
+    print(f'ERROR: {error_message}')
